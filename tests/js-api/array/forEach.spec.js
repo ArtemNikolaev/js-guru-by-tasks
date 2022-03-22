139b891b6d214c74025a462030790b2caef5530f
Artem Nikolaev <artem@nikolaev.by>
2022-03-22 13:21:09 +0300

    testCase: cb should be called for every el

--- tests/js-api/array/forEach.spec.js
+++ tests/js-api/array/forEach.spec.js
@@ -21,27 +21,42 @@ describe.only('tasks/js-api/array/forEach.js \n [].forEach', function() {
         }
     });
 
-/*
     describe('cb', function() {
-        it('first argument should be value', function() {
+        it('should be called for every el', function () {
+            const testCases = [
+                [],
+                [1,2,3],
+                [0,1,2,3],
+                [Symbol()],
+            ]
+
+            for (let i = 0; i < testCases.length; i++) {
+                const counter = callTimes();
+                testCases[i].forEach(counter);
+
+
+                assert.equal(counter(), testCases[i].length);
+            }
+        });
+
+        /*it('first argument should be value', function() {
             const testCase = [1,2,3];
             assert.deepEqual(testCase, testCase.forEach(el=>el));
-        });
+        });*/
 
-        it('second argument should be index', function() {
+        /*it('second argument should be index', function() {
             const testCase = [0,1,2,3];
             assert.deepEqual(testCase, testCase.forEach((el,i)=>i));
-        });
+        });*/
 
-        it('thire argument should be array itself', function() {
+        /*it('thire argument should be array itself', function() {
             const testCase = [0,1,2,3];
             assert.ok(
                 testCase
                     .forEach((el,i,ar)=>ar)
                     .every(el=>el===testCase));
-        });
+        });*/
     });
-*/
 
     it('thisArg should be context for cb', function() {
         const testCases = [
